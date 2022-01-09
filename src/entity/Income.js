6eeac3443cd7e5647a87d8e70bcf9d87a07ed3d7
claudio.onohara <claudioonohara@gmail.com>
2022-01-08 23:25:41 -0400

    feat: service

--- src/entity/Income.js
+++ src/entity/Income.js
@@ -1,4 +1,4 @@
-import languageConfig from '../config/language.js';
+import languageConfig from "../config/language.js";
 
 const defaultLanguage = languageConfig.default;
 
@@ -11,10 +11,10 @@ class Income {
     conversion03,
   }) {
     this.position = position;
-    this.expectation = expectation || { currency: 'BRL', language: 'pt-BR' };
-    this.conversion01 = conversion01 || { currency: 'USD', language: 'en-US' };
-    this.conversion02 = conversion02 || { currency: 'EUR', language: 'en-GB' };
-    this.conversion03 = conversion03 || { currency: 'RUB', language: 'ru-RU' };
+    this.expectation = expectation || { currency: "BRL", language: "pt-BR" };
+    this.conversion01 = conversion01 || { currency: "USD", language: "en-US" };
+    this.conversion02 = conversion02 || { currency: "EUR", language: "en-GB" };
+    this.conversion03 = conversion03 || { currency: "RUB", language: "ru-RU" };
   }
 
   format() {
@@ -31,8 +31,10 @@ class Income {
   static formatCurrency({ currency, value, language }) {
     const _language = language || defaultLanguage;
 
-    // TODO: Implement method
-    return null;
+    return { currency, language, value: new Intl.NumberFormat(_language, {
+      style: "currency",
+      currency,
+    }).format(value)}
   }
 }
 
--- src/service/IncomeService.js
+++ src/service/IncomeService.js
@@ -1,16 +1,38 @@
-import IncomeRepository from './../repository/IncomeRepository.js';
-import Income from './../entity/Income.js';
+import IncomeRepository from "./../repository/IncomeRepository.js";
+import Income from "./../entity/Income.js";
 
 class IncomeService {
   constructor({ incomeRepository } = {}) {
     this.incomeRepository = incomeRepository || new IncomeRepository();
   }
 
-  async generateIncomeFromString(incomeString, delimiter = ';') {
+  async generateIncomeFromString(incomeString, delimiter = ";") {
+    if (!incomeString) {
+      throw new Error(
+        "Position is a required field. Please make sure you are providing a position."
+      );
+    }
+
     const [position, expectation] = incomeString.split(delimiter);
 
-    // @TODO: Implement method
-    return null;
+    if (isNaN(expectation)) {
+      throw new Error(
+        "A valid Expectation is required. Please note that only numbers are allowed."
+      );
+    }
+
+    const income = new Income({ position });
+    income.expectation.value = Number(expectation);
+    const conversions = await this.incomeRepository.getConversions();
+
+    income.conversion01.value =
+      expectation * conversions[income.conversion01.currency];
+    income.conversion02.value =
+      expectation * conversions[income.conversion02.currency];
+    income.conversion03.value =
+      expectation * conversions[income.conversion03.currency];
+
+    return income;
   }
 }
 
